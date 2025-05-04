7dc7af22a2bfee1678efc4d1966447ca7322402e
heiner <heinerDevelops@gmail.com>
2025-05-05 01:39:27 +0200

    ...

--- app/layout.tsx
+++ app/layout.tsx
@@ -23,9 +23,7 @@ export default function RootLayout({
   children: React.ReactNode
 }>) {
   return (
-    <ClerkProvider
-      publishableKey={process.env.NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY}
-    >
+    <ClerkProvider>
       <TRPCProvider>
         <html lang="en">
           <body
