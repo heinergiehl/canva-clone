cfe3b91c887bb372f5e5907b95320007e984568c
heiner <heinerDevelops@gmail.com>
2025-05-05 02:10:08 +0200

    ...

--- next.config.ts
+++ next.config.ts
@@ -8,6 +8,9 @@ const nextConfig: NextConfig = {
     // WARNING: this skips _all_ linting errors at build time
     ignoreDuringBuilds: true,
   },
+  ...(process.env.PRODUCTION
+    ? { basePath: "/myapps/canva-clone", assetPrefix: "/myapps/canva-clone/" }
+    : {}),
 }
 
 export default nextConfig
